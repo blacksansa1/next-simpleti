b011f0fecbc9d44314e614f035595d6a9b1fcc97
blacksansa1 <atendimento.simpleti@gmail.com>
2023-02-02 15:59:58 -0300

    add card

--- src/app/components/second-section.tsx
+++ src/app/components/second-section.tsx
@@ -4,12 +4,16 @@ export default function SecondSection() {
   return (
     <>
       <div className={styles.container}>
-        <h3 className={styles.titleCopy}>Nossa missão</h3>
+        <div className={styles.box}>
+          <div className={styles.titleContainer}>
+            <div className={styles.titleCopy}>Nossa missão</div>
+          </div>
 
-        <div className={styles.copy}>
-          A missão da Simpleti é proporcionar assistência técnica excepcional
-          aos nossos clientes, Oferecendo serviços de alta qualidade no conforto
-          de suas próprias casas.
+          <div className={styles.copy}>
+            A missão da Simpleti é proporcionar assistência técnica excepcional
+            aos nossos clientes, Oferecendo serviços de alta qualidade no
+            conforto de suas próprias casas.
+          </div>
         </div>
       </div>
     </>
--- src/app/components/third-section.tsx
+++ src/app/components/third-section.tsx
@@ -1,13 +1,24 @@
-import style from "../components/third-section.module.css"
+import style from "../components/third-section.module.css";
 
-export default function ThirdSection (){
-    return (
-        <div className={style.container}>
-            <div className={style.textContainer}>
+export default function ThirdSection() {
+  return (
+    <div className={style.container}>
 
-            </div>
+      <div className={style.textContainer}>
 
+        <div className={style.title}>
+            Quem nos somos
+            <img className={style.img} src="/assets/images/squaredots.svg" alt="" />
         </div>
-    )
 
+        <div className={style.copy}>
+          A empresa de Tecnologias da informação que você precisa no seu dia a dia. <br/> 
+          Nosso foco é assistencia de usuarios em suas residencias.<br/>
+          Com profissionais altamente treinados e com experiencia.
+          Resolvemos o seu problema qualquer seja ele. 
+        </div>
+        
+      </div>
+    </div>
+  );
 }
