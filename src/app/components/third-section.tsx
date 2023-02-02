5f7e5cb2a261700b6dfbb0490583c2bc3f83abfa
blacksansa1 <atendimento.simpleti@gmail.com>
2023-02-02 10:12:13 -0300

    400px screen fix

--- src/app/components/third-section.tsx
+++ src/app/components/third-section.tsx
@@ -3,6 +3,9 @@ import style from "../components/third-section.module.css"
 export default function ThirdSection (){
     return (
         <div className={style.container}>
+            <div className={style.textContainer}>
+
+            </div>
 
         </div>
     )
